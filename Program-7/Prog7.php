9e882054bd227b100c344c5273c3eb6d36827963
Badarijitwta <67597010+Badarijitwta@users.noreply.github.com>
2021-07-02 16:14:47 +0530

    Create Prog7.php

---
+++ Program-7/Prog7.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="refresh" content="1" />
+ 
+<style>
+  
+p {
+color: white;
+font-size: 50px;
+position: absolute;
+top: 40%;
+left: 50%;
+transform: translate(-50%, -50%);
+}
+  
+body {
+background-color: blue;
+}
+  
+</style>
+  
+<p> Server time is  <?php echo date(" h:i:s A");?> <br>
+    Today is <?php echo date("Y/m/d");?> <br>
+    Today is <?php echo date("Y.m.d");?> <br>
+    Today is <?php echo date("Y-m-d");?> <br>
+    Today is <?php echo date("l");?>
+</p>
+  
+</head>
+</html>
